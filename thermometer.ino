6aca1dc494ec289f4d072a9eabfd88e5c3a4fc18
infr <infr@iki.fi>
2018-03-03 00:16:19 +0200

    add debug messages, add better UI

--- thermometer.ino
+++ thermometer.ino
@@ -2,23 +2,37 @@
 
 float temp;
 int sensorPin = A1;
-    
+unsigned long time;
+
 void setup() {
     Serial.begin(115200);
     pinMode(sensorPin, INPUT);
+    // digitalWrite(sensorPin, HIGH);
 }
 
 void loop() {
+    //time = millis();
+    //Serial.print("<");
+    //Serial.print(time);  
     temp = getTemp();
     Serial.print("Temperature: ");
-    Serial.println(temp);
+    Serial.print(temp);
+    Serial.println("°C");
     delay(3000);
 }
 
 float getTemp() {
     float reading = analogRead(sensorPin);
+    //Serial.print(": Reading: ");
+    //Serial.print(reading);
     float percent = reading/1023;
+    //Serial.print(", percent: ");
+    //Serial.print(percent);
     float volts = percent*5;
-    return 100*volts;    
+    //Serial.print(", volts: ");
+    //Serial.print(volts);
+    //Serial.print("> ");
+    return volts*100.0;
 }
+
 
